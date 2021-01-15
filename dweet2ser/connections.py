e4cfce469b387ff703fedef5e0a49ce3c07fbfce
Zach <51764547+zhenry9@users.noreply.github.com>
2021-01-14 22:56:32 -0800

    Reworked with pyCharm

--- dweet2ser/connections.py
+++ dweet2ser/connections.py
@@ -1,45 +1,46 @@
-
 # standard imports
 import datetime
 
 # 3rd party imports
 from colorama import init
 from termcolor import colored
+
 # colorama call
 init()
 
-def listen_to_dweet(dweetSesh):
+
+def listen_to_dweet(dweet_sesh):
     """listens for dweets for the given ID and writes to the given serial port.
        CP 545 data is expected as b'TN_0000_0000_01_12:34:56.7890_01234\x09\xDE\xCA\x0D\x0A'
-    """ 
-    ser = dweetSesh.serial_port
-    target = dweetSesh.fromThatDevice
-    for dweet in dweetSesh.listen_for_dweets():     
+    """
+    ser = dweet_sesh.serial_port
+    target = dweet_sesh.fromThatDevice
+    for dweet in dweet_sesh.listen_for_dweets():
         content = dweet["content"]
-        
+
         if target in content:
             output = content[target]
-            if type(output) is not str: # make sure the output is a string
+            if type(output) is not str:  # make sure the output is a string
                 output = str(output)
-            outputBytes = bytes.fromhex(output) # convert dweet string into bytes for RS232.
-            ser.write(outputBytes)
+            output_bytes = bytes.fromhex(output)  # convert dweet string into bytes for RS232.
+            ser.write(output_bytes)
             timestamp = str(datetime.datetime.now())
-            outputText = outputBytes.strip().decode('latin-1')
-            print(timestamp + ":\treceived " + colored("dweet", "cyan")) 
-            print("\t\t\t\t" + outputText) 
-            print("\t\t\t\twritten to " + colored("serial\n", "red"))             
-    
-def listen_to_serial(dweetSesh):
+            output_text = output_bytes.strip().decode('latin-1')
+            print(timestamp + ":\treceived " + colored("dweet", "cyan"))
+            print("\t\t\t\t" + output_text)
+            print("\t\t\t\twritten to " + colored("serial\n", "red"))
+
+
+def listen_to_serial(dweet_sesh):
     """listens to serial port, if it hears something it dweets it
     """
-    ser = dweetSesh.serial_port
-    target = dweetSesh.fromThisDevice
+    ser = dweet_sesh.serial_port
+    target = dweet_sesh.fromThisDevice
     while True:
         if ser.in_waiting > 0:
-            serData = ser.readline()
+            ser_data = ser.readline()
             timestamp = str(datetime.datetime.now())
             print(timestamp + ":\treceived " + colored("serial data", "red"))
-            print("\t\t\t\t " + serData.strip().decode('latin-1'))
+            print("\t\t\t\t " + ser_data.strip().decode('latin-1'))
             print("\t\t\t\tsent to  " + colored("dweet.io\n", "cyan"))
-            dweetSesh.send_dweet({target: serData.hex()})
-                           
+            dweet_sesh.send_dweet({target: ser_data.hex()})
--- dweet2ser/dweetsession.py
+++ dweet2ser/dweetsession.py
@@ -1,4 +1,3 @@
-
 # standard imports
 import time
 import datetime
@@ -8,78 +7,83 @@ from configparser import ConfigParser
 import dweepy
 import serial
 import requests
+from urllib3.exceptions import ProtocolError
+
 
 class DweetSession(object):
 
-    def __init__(self, thingId, key, pc_keyword, device_keyword, port, mode):
-        self.thingId = thingId
+    def __init__(self, thing_id, key, pc_keyword, device_keyword, port, mode):
+        self.thingId = thing_id
         self.key = key
         self.port = port
         self.mode = mode
         self.session = requests.Session()
         self.serial_port = serial.Serial(port)
-        if mode == 'DTE':          # in DTE (pc) mode look for dweets under the device keyword, write dweets under the PC keyword
+
+        # in DTE (pc) mode look for dweets under the device keyword, write dweets under the PC keyword
+        if mode == 'DTE':
             self.fromThisDevice = pc_keyword
             self.fromThatDevice = device_keyword
-        
-        elif mode == 'DCE':        # in DCE (device) mode look for dweets under the PC keyword, write dweets under the device keyword
+
+        # in DCE (device) mode look for dweets under the PC keyword, write dweets under the device keyword
+        elif mode == 'DCE':
             self.fromThisDevice = device_keyword
             self.fromThatDevice = pc_keyword
-        
+
     @classmethod
-    def from_config_file(cls, configFile, port, mode):
+    def from_config_file(cls, config_file, port, mode):
         # open our config file
         cfg = ConfigParser()
-        cfg.read(configFile)
-        
+        cfg.read(config_file)
+
         # get thing ID from config file 
-        thingId = cfg.get("thing", "id")
-        thingKey = cfg.get("thing", "key")
-        if thingKey == 'None' or thingKey == '':
-            thingKey = None
-        
+        thing_id = cfg.get("thing", "id")
+        thing_key = cfg.get("thing", "key")
+        if thing_key == 'None' or thing_key == '':
+            thing_key = None
+
         # get some defaults out of the config file
         default_pc_keyword = cfg.get("defaults", "pc_buffer")
         default_device_buffer = cfg.get("defaults", "device_buffer")
-        
+
         if port is None:
             if mode == 'DTE':
                 port = cfg.get("defaults", "DTE_port")
             if mode == 'DCE':
                 port = cfg.get("defaults", "DCE_port")
-        
-        return cls(thingId, thingKey, default_pc_keyword, default_device_buffer, port, mode)
-        
+
+        return cls(thing_id, thing_key, default_pc_keyword, default_device_buffer, port, mode)
+
     def restart_session(self):
-        ''' starts a new requests session
-        '''
+        """ starts a new requests session
+        """
         self.session = requests.Session()
-        
+
     def send_dweet(self, content):
         try:
-            dweepy.dweet_for(self.thingId, content, key = self.key, session = self.session)
-        
+            dweepy.dweet_for(self.thingId, content, key=self.key, session=self.session)
+
         except dweepy.DweepyError as e:
             print(e)
             timestamp = str(datetime.datetime.now())
             print(timestamp + ":\tTrying again...")
             time.sleep(2)
             return self.send_dweet(content)
-            
+
         except (ConnectionError, ProtocolError, WindowsError) as e:
             print(e.response)
             timestamp = str(datetime.datetime.now())
             print(timestamp + ":\tConnection closed by dweet, restarting:")
             self.restart_session()
             return self.send_dweet(content)
-    
+
     def listen_for_dweets(self):
-        ''' makes a call to dweepy to start a listening stream. error handling needs work
-        '''
+        """ makes a call to dweepy to start a listening stream. error handling needs work
+        """
         try:
-            for dweet in dweepy.listen_for_dweets_from( self.thingId, key=self.key, timeout=90000, session=self.session ):
+            for dweet in dweepy.listen_for_dweets_from(self.thingId, key=self.key, timeout=90000, session=self.session):
                 yield dweet
-                
+
         # if you get an error because dweet closed the connection, open it again.
         except (ConnectionError, ProtocolError, WindowsError) as e:
             print(e.response)
@@ -87,13 +91,11 @@ class DweetSession(object):
             print(timestamp + ":\tConnection closed by dweet, restarting:")
             self.restart_session()
             return self.listen_for_dweets()
-        
+
     def keepalive(self):
-        """ dweet.io seems to close the connection after 60 seconds of inactivity. This sends a dummy payload every 45s to avoid that. 
+        """ dweet.io seems to close the connection after 60 seconds of inactivity.
+            This sends a dummy payload every 45s to avoid that.
         """
         while True:
             time.sleep(45)
             self.send_dweet({"keepalive": 1})
-                
-        
-        
