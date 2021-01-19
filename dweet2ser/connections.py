c649e7c719e41f8707a7e36e0599349cf3447d16
Zach <51764547+zhenry9@users.noreply.github.com>
2021-01-18 18:01:32 -0800

    merged connections file into DweetSession object to simplify things

--- dweet2ser/connections.py
+++ dweet2ser/connections.py
@@ -1,34 +1,46 @@
 # standard imports
 import datetime
+import multiprocessing
 import queue
-import threading
 import time
-
-# 3rd party imports
-
 
 from colorama import init
 from termcolor import colored
+
+# 3rd party imports
 
 # colorama call
 init()
 
 
-def _listen(dweet_sesh, target, ser):
+def _write_to_serial(output, ser):
+    if type(output) is not str:  # make sure the output is a string
+        output = str(output)
+    output_bytes = bytes.fromhex(output)  # convert dweet string into bytes for RS232.
+    ser.write(output_bytes)
+    timestamp = str(datetime.datetime.now())
+    output_text = output_bytes.strip().decode('latin-1')
+    print(timestamp + ":\treceived " + colored("dweet", "cyan"))
+    print("\t\t\t\t" + output_text)
+    print("\t\t\t\twritten to " + colored("serial\n", "red"))
+
+
+def _write_last_dweet(dweet_sesh):
+    ser = dweet_sesh.serial_port
+    target = dweet_sesh.fromThatDevice
+    dweet: dict = dweet_sesh.get_latest_dweet()
+    content = dweet[0]["content"]
+    if target in content:
+        _write_to_serial(content[target], ser)
+
+
+def _listen(dweet_sesh):
+    ser = dweet_sesh.serial_port
+    target = dweet_sesh.fromThatDevice
     for dweet in dweet_sesh.listen_for_dweets():
         content = dweet["content"]
-
         if target in content:
-            output = content[target]
-            if type(output) is not str:  # make sure the output is a string
-                output = str(output)
-            output_bytes = bytes.fromhex(output)  # convert dweet string into bytes for RS232.
-            ser.write(output_bytes)
-            timestamp = str(datetime.datetime.now())
-            output_text = output_bytes.strip().decode('latin-1')
-            print(timestamp + ":\treceived " + colored("dweet", "cyan"))
-            print("\t\t\t\t" + output_text)
-            print("\t\t\t\twritten to " + colored("serial\n", "red"))
+            _write_to_serial(content[target], ser)
 
 
 def listen_to_dweet(dweet_sesh, bucket):
@@ -36,19 +48,19 @@ def listen_to_dweet(dweet_sesh, bucket):
     Starts listening for dweets. Watches the bucket and restarts the thread if a crash message is received.
     """
     # TODO: this seems pretty ugly. Figure out why stream crashes silently and find a better fix
-    ser = dweet_sesh.serial_port
-    target = dweet_sesh.fromThatDevice
 
-    t = threading.Thread(target=_listen, args=[dweet_sesh, target, ser])
+    t = multiprocessing.Process(target=_listen, args=[dweet_sesh])
     t.daemon = True
     t.start()
     while True:
         try:
+            # if another thread wrote a crash message its time to restart the stream
             if bucket.get(block=False) == 'crash':
+                t.kill()
                 timestamp = str(datetime.datetime.now())
                 print(timestamp + ":\tListen thread crashed, restarting.")
-                t = threading.Thread(target=_listen, args=[dweet_sesh, target, ser])
-                t.daemon = True
+                # retrieve the last dweet and send to serial, in case it was missed during crash
+                _write_last_dweet(dweet_sesh)
                 t.start()
         except queue.Empty:
             time.sleep(.01)
